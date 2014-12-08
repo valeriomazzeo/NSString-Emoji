994e58095cc443b01e7d287e864270500791686d
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2014-12-08 22:00:55 +0000

    fixed property 'firstObject' not found

--- NSString+Emoji.m
+++ NSString+Emoji.m
@@ -891,7 +891,7 @@ static NSDictionary * s_cheatCodesToUnicode = nil;
     if (self.length) {
         __block NSMutableString *newText = [NSMutableString stringWithString:self];
         [s_unicodeToCheatCodes enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-            NSString *string = ([obj isKindOfClass:[NSArray class]] ? obj.firstObject : obj);
+            NSString *string = ([obj isKindOfClass:[NSArray class]] ? [obj firstObject] : obj);
             [newText replaceOccurrencesOfString:key withString:string options:NSLiteralSearch range:NSMakeRange(0, newText.length)];
         }];
         return newText;
